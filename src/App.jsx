fba718ee567e7373e1df5f3e06ed6c36023fe483
Sahil Puniyani <punyanisahil0001@gmail.com>
2025-06-06 03:02:30 +0530

    Update the table ui and change the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 import React, { useState } from 'react';
 
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router';
 import MainLayout from './screens/MainLayout';
 import NewRequest from './screens/NewRequest';
 import Estimate from './screens/Estimate';
@@ -9,14 +9,12 @@ import EventDetails from './screens/EventDetails';
 
 const App = () => {
 
-
-
-
   const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
+        { index: true, element: <Navigate to="/new-requests" replace /> },
       { path: "new-requests", element: <NewRequest /> },
       { path: "estimate", element: <Estimate /> },
       { path: "/event/:id", element: <EventDetails /> },
